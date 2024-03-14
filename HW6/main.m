5708ca8f75da5099491ec7ebb95308327946e0ac
PetrashTank <zanka_16@mail.ru>
2024-03-14 18:12:06 +0300

    Initial Commit

---
+++ HW6/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HW6
+//
+//  Created by User on 14.03.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
